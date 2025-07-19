78a8a7440c31c95162b30051390b95d504d309e1
Mykyta Musaiev <n.musaiev@gmail.com>
2025-07-19 23:09:01 +0300

    task done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
 // write code here
+
+const ulList = document.querySelectorAll('li > ul');
+
+ulList.forEach((ul) => {
+  const li = ul.parentElement;
+  const textNode = li.firstChild;
+  const span = document.createElement('span');
+
+  textNode.replaceWith(span);
+  span.textContent = textNode.textContent;
+});
+
+const mainUl = document.querySelector('.tree');
+
+mainUl.addEventListener('click', (e) => {
+  if (e.target.tagName === 'SPAN') {
+    const span = e.target;
+    const nestedUl = span.nextElementSibling;
+
+    if (nestedUl) {
+      if (nestedUl.style.display === 'none') {
+        nestedUl.style.display = '';
+      } else {
+        nestedUl.style.display = 'none';
+      }
+    }
+  }
+});
